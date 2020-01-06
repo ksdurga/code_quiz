c1f6bc3c1c95764332d3cb94d7aff1ff5843d05f
Katie Durga <katie.durga@gmail.com>
2020-01-06 14:39:20 -0500

    created questions and answers

---
+++ assets/javascript/script.js
@@ -0,0 +1,94 @@
+//questions and answers
+var questions = [
+   //1
+   {
+     title: "Commonly used data types DO NOT include:",
+     choices: ["strings", "booleans", "alerts", "numbers"],
+     answer: "alerts"
+   },
+   //2
+   {
+     title: "The condition in an if / else statement is enclosed within ____.",
+     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+     answer: "parentheses"
+   },
+   //3
+   {
+      title: "What is the name of the statement that is used to exit or end a loop?",
+      choices: ["close statement", "falter statement", "conditional statement", "break statement"],
+      answer: "break statement"
+   },
+   //4
+   {
+      title: "What is the language or list of instructions that are executed by the computer (how JavaScript is built)?",
+      choices: ["syntax", "JSON", "scope", "output"],
+      answer: "syntax"
+   },
+   //5
+   {   
+      title: "What is the format called that is used for storing and transporting data?",
+      choices: ["font", "syntax", "HTML", "JSON"],
+      answer: "JSON"
+   },
+   //6
+   {
+      title: "What is the element used – and hidden – in code that explains things and makes the content more readable?",
+      choices: ["comments", "quotations", "comparisons", "notes"],
+      answer: "comments"
+   },
+   //7
+   {
+      title: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+      choices: ["else loop", "conditional loop", "while loop", "for loop"],
+      answer: "while loop"
+   },
+   //8
+   {
+      title: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+      choices: ["strings", "arrays", "variables", "recorders"],
+      answer: "strings"
+   },
+   //9
+   {
+      title: "In JavaScript, what element is used to store multiple values in a single variable?",
+      choices: ["functions", "arrays", "strings", "variables"],
+      answer: "arrays"
+   },
+   //10
+   {
+      title: "What kind of statement is used to execute actions based on a trigger or condition?",
+      choices: ["fired event", "boolean variable", "regular expression", "conditional statement"],
+      answer: "conditional statement"
+   },
+   //11
+   {
+      title: "What is the object called that lets you work with both dates and time-related data?",
+      choices: ["dates", "clock", "time-warp", "time-field"],
+      answer: "dates"
+   },
+   //12
+   {
+      title: "What is considered to be the most popular programming language in the world?",
+      choices: ["HTML", "ruby", "swift", "JavaScript"],
+      answer: "JavaScript"
+   },
+   //13
+   {
+      title: "What tag is used to define a container for an external app or plug-in in HTML?",
+      choices: ["<!DOCTYPE>", "<caption>", "<code>", "<embed>"],
+      answer: "<embed>"
+   },
+   //14
+   {
+      title: "What tag is used to define a list item (in a bulleted list)?",
+      choices: ["<li>", "<s>", "<u>", "<ul>"],
+      answer: "<li>"
+   },
+   //15
+   {
+      title: "What tag is used to specify a section of text that provides an example of computer code?",
+      choices: ["<embed>", "<!DOCTYPE", "<caption>", "<code>"],
+      answer: "<code>"
+   }
+ ];
+ 
