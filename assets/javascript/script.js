9364939451333f17d712afeead714e64a54d7f33
Katie Durga <katie.durga@gmail.com>
2020-01-07 21:45:50 -0500

    edited question list

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,94 +1,204 @@
+$(document).ready(function() {
 //questions and answers
-var questions = [
+var quizArr = [
    //1
-   {
-     title: "Commonly used data types DO NOT include:",
-     choices: ["strings", "booleans", "alerts", "numbers"],
-     answer: "alerts"
-   },
+   { 
+      question: "Commonly used data types DO NOT include:",
+         answers: [
+         {text: "Strings", correct: false},
+         {text: "Booleans", correct: false},
+         {text: "Alerts", correct: true},
+         {text: "Numbers", correct: false}
+         ],
+      },
+   
    //2
    {
-     title: "The condition in an if / else statement is enclosed within ____.",
-     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-     answer: "parentheses"
+      question: "The condition in an if / else statement is enclosed within ____.",
+         answers: [
+         {text: "Quotes", correct: false},
+         {text: "Curly Brackets", correct: false},
+         {text: "Parenthesis", correct: true},
+         {text: "Square Brackets", correct: false}
+         ],
    },
    //3
    {
-      title: "What is the name of the statement that is used to exit or end a loop?",
-      choices: ["close statement", "falter statement", "conditional statement", "break statement"],
-      answer: "break statement"
+      question: "What is the name of the statement that is used to exit or end a loop?",
+         answers: [
+         {text: "Close Statement", correct: false},
+         {text: "Falter Statement", correct: false},
+         {text: "Conditional Statement", correct: false},
+         {text: "Break Statement", correct: true}
+         ],
    },
    //4
    {
-      title: "What is the language or list of instructions that are executed by the computer (how JavaScript is built)?",
-      choices: ["syntax", "JSON", "scope", "output"],
-      answer: "syntax"
+      question: "What is the language or list of instructions that are executed by the computer (how JavaScript is built)?",
+         answers: [
+         {text: "Syntax", correct: true},
+         {text: "JSON", correct: false},
+         {text: "Scope", correct: false},
+         {text: "Output", correct: false}
+         ],
    },
    //5
    {   
-      title: "What is the format called that is used for storing and transporting data?",
-      choices: ["font", "syntax", "HTML", "JSON"],
-      answer: "JSON"
+      question: "What is the format called that is used for storing and transporting data?",
+         answers: [
+         {text: "Font", correct: false},
+         {text: "Syntax", correct: false},
+         {text: "HTML", correct: false},
+         {text: "JSON", correct: true}
+         ],
    },
    //6
    {
-      title: "What is the element used – and hidden – in code that explains things and makes the content more readable?",
-      choices: ["comments", "quotations", "comparisons", "notes"],
-      answer: "comments"
+      question: "What is the element used – and hidden – in code that explains things and makes the content more readable?",
+         answers: [
+         {text: "Comments", correct: true},
+         {text: "Quotations", correct: false},
+         {text: "Comparisons", correct: false},
+         {text: "Notes", correct: false}
+         ],
    },
    //7
    {
-      title: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
-      choices: ["else loop", "conditional loop", "while loop", "for loop"],
-      answer: "while loop"
+      question: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+         answers: [
+         {text: "Else Loop", correct: false},
+         {text: "Conditional Loop", correct: false},
+         {text: "While Loop", correct: true},
+         {text: "For Loop", correct: false}
+         ],
    },
    //8
    {
-      title: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
-      choices: ["strings", "arrays", "variables", "recorders"],
-      answer: "strings"
+      question: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+         answers: [
+         {text: "Strings", correct: true},
+         {text: "Arrays", correct: false},
+         {text: "Variable", correct: false},
+         {text: "Recorders", correct: false}
+         ],
    },
    //9
    {
-      title: "In JavaScript, what element is used to store multiple values in a single variable?",
-      choices: ["functions", "arrays", "strings", "variables"],
-      answer: "arrays"
+      question: "In JavaScript, what element is used to store multiple values in a single variable?",
+         answers: [
+         {text: "Functions", correct: false},
+         {text: "Arrays", correct: true},
+         {text: "Strings", correct: false},
+         {text: "Variables", correct: false}
+         ],
    },
    //10
    {
-      title: "What kind of statement is used to execute actions based on a trigger or condition?",
-      choices: ["fired event", "boolean variable", "regular expression", "conditional statement"],
-      answer: "conditional statement"
+      question: "What kind of statement is used to execute actions based on a trigger or condition?",
+         answers: [
+         {text: "Fired Event", correct: false},
+         {text: "Boolean Variable", correct: false},
+         {text: "Regular Expression", correct: false},
+         {text: "Conditional Statement", correct: true}
+         ],
    },
    //11
    {
-      title: "What is the object called that lets you work with both dates and time-related data?",
-      choices: ["dates", "clock", "time-warp", "time-field"],
-      answer: "dates"
+      question: "What is the object called that lets you work with both dates and time-related data?",
+         answers: [
+         {text: "Dates", correct: false},
+         {text: "Clock", correct: false},
+         {text: "Time-warp", correct: false},
+         {text: "Time-field", correct: true}
+         ],
    },
    //12
    {
-      title: "What is considered to be the most popular programming language in the world?",
-      choices: ["HTML", "ruby", "swift", "JavaScript"],
-      answer: "JavaScript"
+      question: "What is considered to be the most popular programming language in the world?",
+         answers: [
+         {text: "HTML", correct: false},
+         {text: "Ruby", correct: false},
+         {text: "Swift", correct: false},
+         {text: "JavaScript", correct: true}
+         ],
    },
    //13
    {
-      title: "What tag is used to define a container for an external app or plug-in in HTML?",
-      choices: ["<!DOCTYPE>", "<caption>", "<code>", "<embed>"],
-      answer: "<embed>"
+      question: "What tag is used to define a container for an external app or plug-in in HTML?",
+         answers: [
+         {text: "<!DOCTYPE>", correct: false},
+         {text: "<caption>", correct: false},
+         {text: "<code>", correct: false},
+         {text: "<embed>", correct: true}
+         ],
    },
    //14
    {
-      title: "What tag is used to define a list item (in a bulleted list)?",
-      choices: ["<li>", "<s>", "<u>", "<ul>"],
-      answer: "<li>"
+      question: "What tag is used to define a list item (in a bulleted list)?",
+         answers: [
+         {text: "<li>", correct: false},
+         {text: "<s>", correct: false},
+         {text: "<u>", correct: false},
+         {text: "<ul>", correct: true}
+         ],
    },
    //15
    {
-      title: "What tag is used to specify a section of text that provides an example of computer code?",
-      choices: ["<embed>", "<!DOCTYPE", "<caption>", "<code>"],
-      answer: "<code>"
+      question: "What tag is used to specify a section of text that provides an example of computer code?",
+         answers: [
+         {text: "<embed>", correct: false},
+         {text: "<!DOCTYPE>", correct: false},
+         {text: "<caption>", correct: false},
+         {text: "<code>", correct: true}
+         ],
    }
  ];
+
+   var home = $("#homepage");
+   var startBtn = $("#start-btn");
+   var quizDiv = $("#quiz");
+   var time = $("#timer");
+   
+   /*PSEUDO CODE
+   create element that displays quiz questions and answers
+   once answer is selected display next question and answer set
+   if answer selected matches correct answer do nothing else subtract 5 seconds from timer
+   */
+
+   var questionElement = $("#quiz")
+   var questionCount = 0;
+   var secondsLeft = 0;
+   var currentScore = 0;
+   var highScores = [];
+
+//   function displayQuestion(num) {
+//    home.hide();
+//    $("#questionDisplay").text(questions[num].title);
  
+//    for (var i = 0; i < questions[num].choices.length; i++) {
+//      $("#optionDisplay").append("<li class='list-inline-item border-0 mb-n2'><a class='choice btn btn-primary' tabindex=" + i + ">" + questions[num].choices[i]);
+//    }
+//  }
+
+   startBtn.on("click", function(e) {
+      e.preventDefault();
+      console.log("click");
+      // displayQuestion(questionCount);
+      showQuestion();
+   });
+   
+   function showQuestion(question) {
+      questionElement.innerHTML = questions.questions
+      questions.choices.forEach(answer => {
+        var button = document.createElement('button');
+        button.innerHTML = choices.text;
+        button.classList.add('btn');
+        if( choices.answer ) {
+          button.dataset.correct = answer.correct;
+        } else {
+          button.dataset.correct = answer.wrong;
+        }
+        // button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+      })};
+});
